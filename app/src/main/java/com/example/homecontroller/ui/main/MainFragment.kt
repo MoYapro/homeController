a6f6c96318b7c5d0bee1b1fb510a07345a07f6a6
ritmeier <tom.ritmeier@gmail.com>
2019-11-09 10:25:55 +0100

    replace fragment_layout with programmatic layout + add button

--- app/src/main/java/com/example/homecontroller/ui/main/MainFragment.kt
+++ app/src/main/java/com/example/homecontroller/ui/main/MainFragment.kt
@@ -6,7 +6,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.homecontroller.R
+import android.widget.Button
+import android.widget.RelativeLayout
+import android.widget.TextView
+
 
 class MainFragment : Fragment() {
 
@@ -16,9 +19,36 @@ class MainFragment : Fragment() {
 
     private lateinit var viewModel: MainViewModel
 
-    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?,
-                              savedInstanceState: Bundle?): View {
-        return inflater.inflate(R.layout.main_fragment, container, false)
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View {
+        val layout: RelativeLayout = RelativeLayout(this.context)
+        // Defining the RelativeLayout layout parameters.
+        // In this case I want to fill its parent
+        val rlp = RelativeLayout.LayoutParams(
+            RelativeLayout.LayoutParams.FILL_PARENT,
+            RelativeLayout.LayoutParams.FILL_PARENT
+        )
+
+        // Creating a new TextView
+        val button = Button(this.context)
+        button.text = "Button"
+
+        // Defining the layout parameters of the TextView
+        val lp = RelativeLayout.LayoutParams(
+            RelativeLayout.LayoutParams.WRAP_CONTENT,
+            RelativeLayout.LayoutParams.WRAP_CONTENT
+        )
+        lp.addRule(RelativeLayout.CENTER_IN_PARENT)
+
+        // Setting the parameters on the TextView
+        button.layoutParams = lp
+
+        // Adding the TextView to the RelativeLayout as a child
+        layout.addView(button)
+        return layout.rootView
     }
 
     override fun onActivityCreated(savedInstanceState: Bundle?) {
