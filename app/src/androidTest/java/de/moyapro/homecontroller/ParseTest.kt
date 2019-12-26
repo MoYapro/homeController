5c6795e9c03871442b12c835e5e55979e0bba34f
Tom Ritmeierr <tom.ritmeier@gmail.com>
2019-12-26 08:32:56 +0100

    add parsing test for tvResponse

--- app/src/androidTest/java/de/moyapro/homecontroller/ParseTest.kt
+++ app/src/androidTest/java/de/moyapro/homecontroller/ParseTest.kt
@@ -1,11 +1,13 @@
 package de.moyapro.homecontroller
 
+import de.moyapro.homecontroller.tvapi.model.TvResponse
 import de.moyapro.homecontroller.tvapi.model.VolumeInformation
 import kotlinx.serialization.ImplicitReflectionSerializer
 import kotlinx.serialization.Serializable
 import kotlinx.serialization.json.Json
 import kotlinx.serialization.parse
 import org.junit.Test
+import org.junit.Assert.*
 
 
 @Serializable
@@ -15,6 +17,18 @@ class ParseTest {
     @UseExperimental(ImplicitReflectionSerializer::class)
     @Test
     fun parseVolumeInformation() {
-        val ex = Json.parse<VolumeInformation>("{\"target\":\"speaker\",\"volume\":3,\"mute\":false,\"maxVolume\":100,\"minVolume\":0}")
+        val ex =
+            Json.parse<VolumeInformation>("{\"target\":\"speaker\",\"volume\":3,\"mute\":false,\"maxVolume\":100,\"minVolume\":0}")
     }
+
+    @UseExperimental(ImplicitReflectionSerializer::class)
+    @Test
+    fun parseResponseWithVolumeInformation() {
+        val expectedVolume = 3
+        val ex =
+            Json.parse<TvResponse>("""{"result":[[{"target":"speaker","volume":$expectedVolume,"mute":false,"maxVolume":100,"minVolume":0}]],"id":20}""")
+        val actualVolume: Int = ex.result[0][0].volume
+        assertEquals("Should have got the correct volume", expectedVolume, actualVolume)
+    }
+
 }
--- app/src/main/java/de/moyapro/homecontroller/tvapi/model/TvResponse.kt
+++ app/src/main/java/de/moyapro/homecontroller/tvapi/model/TvResponse.kt
@@ -1,5 +1,9 @@
 package de.moyapro.homecontroller.tvapi.model
 
+import kotlinx.serialization.Serializable
+
+@Serializable
 data class TvResponse(
-    val result: List<VolumeInformation>
+    val result: List<List<VolumeInformation>>,
+    val id: Int
 )
