6e1f3b24d912d51145963d8963cfdde86c485d78
Tom Ritmeierr <tom.ritmeier@gmail.com>
2020-01-12 07:59:08 +0100

    remove view from MainActivity.kt

--- app/src/main/java/de/moyapro/homecontroller/MainActivity.kt
+++ app/src/main/java/de/moyapro/homecontroller/MainActivity.kt
@@ -1,20 +1,16 @@
 package de.moyapro.homecontroller
 
+import android.content.Intent
 import android.os.Bundle
-import android.view.View
 import androidx.appcompat.app.AppCompatActivity
-import de.moyapro.homecontroller.ui.main.MainFragment
+import de.moyapro.homecontroller.ui.controller.ControllerActivity
 
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.main_activity)
-        if (findViewById<View>(R.id.container) != null && savedInstanceState == null) {
-            supportFragmentManager.beginTransaction()
-                .add(R.id.container, MainFragment.newInstance())
-                .commit()
-        }
+        startActivity(Intent(this, ControllerActivity::class.java))
     }
+
 }
