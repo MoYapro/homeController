1ce5b4fd4f401206860ee3a4afaca5c19ff38931
Tom Ritmeier <tom.ritmeier@iits-consulting.de>
2022-10-13 08:23:59 +0200

    improve hdmi parsing

--- app/src/test/java/de/moyapro/homecontroller/SerializeHdmiTest.kt
+++ app/src/test/java/de/moyapro/homecontroller/SerializeHdmiTest.kt
@@ -6,21 +6,28 @@ import de.moyapro.homecontroller.config.getConfiguredJson
 import io.kotest.matchers.collections.shouldHaveSize
 import io.kotest.matchers.shouldBe
 import kotlinx.serialization.decodeFromString
-import kotlinx.serialization.json.Json
 import org.junit.Test
 
-class SerializeHdmiText {
+class SerializeHdmiTest {
 
     val hdmiStatusResponseExample = """{
     "result": [
         [
             {
+                "uri": "extInput:cec?type=player&port=1&logicalAddr=4",
+                "title": "NintendoSwitch",
+                "connection": true,
+                "label": "",
+                "icon": "meta:playbackdevice",
+                "status": ""
+            },
+            {
                 "uri": "extInput:hdmi?port=1",
                 "title": "HDMI 1",
-                "connection": false,
+                "connection": true,
                 "label": "",
                 "icon": "meta:hdmi",
-                "status": "false"
+                "status": "true"
             },
             {
                 "uri": "extInput:hdmi?port=2",
@@ -72,17 +79,20 @@ class SerializeHdmiText {
     fun parseHdmiJson() {
         val result: HdmiStatusResponse =
             getConfiguredJson().decodeFromString(hdmiStatusResponseExample)
-        result.result.first() shouldHaveSize 6
+        result.result.first() shouldHaveSize 7
         result.result.first().filter { it.uri.contains("hdmi") } shouldHaveSize 4
+        result.result.first().filter { it.title.contains("Switch") } shouldHaveSize 1
         result.result.first().filter { it.uri.contains("composite") } shouldHaveSize 1
         result.result.first().filter { it.uri.contains("widi") } shouldHaveSize 1
-        result.result.first().filter { it.uri.contains("extInput:hdmi?port=2") }
-            .single() shouldBe HdmiStatus(uri = "extInput:hdmi?port=2",
+        result.result.first()
+            .single { it.uri.contains("extInput:hdmi?port=2") } shouldBe HdmiStatus(
+            uri = "extInput:hdmi?port=2",
             title = "HDMI 2",
             connection = false,
             label = "",
             icon = "meta:hdmi",
-            status = "false")
+            status = "false"
+        )
     }
 
 }
